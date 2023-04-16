ef4d1669d33037902a34589b256f94f0c0eda760
amandaguan-ag <amandaguan1314@gmail.com>
2023-04-16 14:30:46 -0700

    update template

---
+++ ProjectName.Tests/ModelTests/ClassTests.cs
@@ -0,0 +1,18 @@
+namespace ProjectName.Tests
+{
+    [TestClass]
+    public class ClassNameTests
+    {
+        [TestMethod]
+        public void MethodName_DescriptionOfBehavior_ExpectedReturnValue()
+        {
+            // Any necessary logic to prep for test; instantiating new classes, etc.
+            // Example:
+            // ClassName newClassName = new ClassName();
+            // Act
+            // var result = newClassName.MethodName();
+            // Assert
+            // Assert.AreEqual(EXPECTED RESULT, result);
+        }
+    }
+}
